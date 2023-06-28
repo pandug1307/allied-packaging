dc5d62a0a185527192207b79315abd00b7b9b592
umesh.pandey <umesh.pandey@softwebsolutions.com>
2023-06-28 15:23:42 +0530

    done

---
+++ cypress/e2e/Loginlogout.cy.js
@@ -0,0 +1,33 @@
+/// <reference types="Cypress"/>
+
+import Loginlogout_PO from "../integration/pageObjects/Loginlogout_PO"
+
+describe('Testcase for login logout ', () => {
+    const loginlogout_PO = new Loginlogout_PO();
+
+    beforeEach(function () {
+        cy.fixture('example').then(function (data) {
+            globalThis.data = data;
+        })
+    })
+
+    it('Verify Invalid credentials', () => {
+		loginlogout_PO.navigate(globalThis.data.login_URL);
+        loginlogout_PO.verify_InvalidLogin(globalThis.data.invalid_username, globalThis.data.invalid_password);
+    });
+
+    it('Verify Valid credentials', () => {
+		loginlogout_PO.navigate(globalThis.data.login_URL);
+        loginlogout_PO.verify_validLogin(globalThis.data.valid_username, globalThis.data.valid_password);
+		loginlogout_PO.dashboard();
+        loginlogout_PO.logout();
+    });
+
+});
+
+
+
+
+
+
+
